7ea4b74ca4b9c917a3ee7b49a6ca5a369b5d7265
Ammad Aslam <ammad.aslam@devbox.co>
2020-07-22 14:30:39 +0500

    displaying msg

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Message.create(body: "i am good", user: User.first)
+Message.create(body: "what about you?", user: User.last)
+Message.create(body: "doing great!", user: User.first)
