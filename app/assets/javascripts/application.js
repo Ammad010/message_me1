0c0e3823b75f4867b499b6a164036eb0d7a764e4
Ammad Aslam <ammad.aslam@devbox.co>
2020-07-22 13:47:18 +0500

    login page added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,14 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+// Loads all Semantic javascripts
+//= require semantic-ui
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+    $('.ui.dropdown').dropdown();
+})
